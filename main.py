607a76ae26ba515a9590864f01d46f0877dae873
vimukthi <121391615+whoiams@users.noreply.github.com>
2023-06-01 04:51:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,131 +1,54 @@
-# Copyright 2021 TerminalWarlord under the terms of the MIT
-# license found at https://github.com/TerminalWarlord/TikTok-Downloader-Bot/blob/master/LICENSE
-# Encoding = 'utf-8'
-# Fork and Deploy, do not modify this repo and claim it yours
-# For collaboration mail me at dev.jaybee@gmail.com
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent
-import shutil
-import requests
-import json
 import os
-import re
-from bs4 import BeautifulSoup as bs
-import time
-from datetime import timedelta
-import math
-import base64
-from progress_bar import progress, TimeFormatter, humanbytes
-from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler
+import requests
 
-load_dotenv()
-bot_token = os.environ.get('BOT_TOKEN')
-workers = int(os.environ.get('WORKERS'))
-api = int(os.environ.get('API_KEY'))
-hash = os.environ.get('API_HASH')
-chnnl = os.environ.get('CHANNEL_URL')
-BOT_URL = os.environ.get('BOT_URL')
-app = Client("JayBee", bot_token=bot_token, api_id=api, api_hash=hash, workers=workers)
+# Define your Telegram bot token
+TOKEN = '6162835664:AAF82yhi5W7jJe8VJxeLTk10xKGCLWBn6Fk'
 
+# Define the command handler for the /start command
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the TikTok downloader bot!")
 
+# Define the command handler for the /download command
+def download(update, context):
+    # Check if a URL is provided as an argument
+    if len(context.args) > 0:
+        url = context.args[0]
+        
+        # Send a message indicating the download has started
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Downloading TikTok video...")
+        
+        # Download the TikTok video using requests library
+        response = requests.get(url, stream=True)
+        
+        # Get the filename from the URL
+        filename = os.path.basename(url)
+        
+        # Save the video to a file
+        with open(filename, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    file.write(chunk)
+        
+        # Send the downloaded video file
+        context.bot.send_video(chat_id=update.effective_chat.id, video=open(filename, 'rb'))
+        
+        # Remove the downloaded video file
+        os.remove(filename)
+        
+    else:
+        # Send an error message if no URL is provided
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a TikTok video URL.")
 
-@app.on_message(filters.command('start'))
-def start(client, message):
-    kb = [[InlineKeyboardButton('Channel 🛡', url=chnnl)]]
-    reply_markup = InlineKeyboardMarkup(kb)
-    app.send_message(chat_id=message.from_user.id, text=f"Hello there, I am **TikTok Downloader Bot**.\nI can download TikTok video without Watermark.\n\n"
-                          "__**Developer :**__ __@vimukthioshada__\n"
-                          "__**Language :**__ __Python__\n"
-                          "__**Framework :**__ __🔥 Pyrogram__",
-                     parse_mode='md',
-                     reply_markup=reply_markup)
+# Create an instance of the Updater class and pass it the Telegram bot token
+updater = Updater(token=TOKEN, use_context=True)
 
+# Get the dispatcher to register handlers
+dispatcher = updater.dispatcher
 
+# Register the command handlers
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("download", download))
 
-
-@app.on_message(filters.command('help'))
-def help(client, message):
-    kb = [[InlineKeyboardButton('Channel 🛡', url=chnnl),]]
-    reply_markup = InlineKeyboardMarkup(kb)
-    app.send_message(chat_id=message.from_user.id, text=f"Hello there, I am **TikTok Downloader Bot**.\nI can download any TikTok video from a given link.\n\n"
-                                            "__Send me a TikTok video link__",
-                     parse_mode='md',
-                     reply_markup=reply_markup)
-
-
-@app.on_message((filters.regex("http://")|filters.regex("https://")) & (filters.regex('tiktok')|filters.regex('douyin')))
-def tiktok_dl(client, message):
-    a = app.send_message(chat_id=message.chat.id,
-                         text='__Downloading File to the Server__',
-                         parse_mode='md')
-    link = re.findall(r'\bhttps?://.*[(tiktok|douyin)]\S+', message.text)[0]
-    link = link.split("?")[0]
-
-
-
-    
-    params = {
-      "link": link
-    }
-    headers = {
-      'x-rapidapi-host': "tiktok-info.p.rapidapi.com",
-      'x-rapidapi-key': "f9d65af755msh3c8cac23b52a5eep108a33jsnbf7de971bb72"
-    }
-    
-    ### Get your Free TikTok API from https://rapidapi.com/TerminalWarlord/api/tiktok-info/
-    #Using the default one can stop working any moment 
-    
-    api = f"https://tiktok-info.p.rapidapi.com/dl/"
-    r = requests.get(api, params=params, headers=headers).json()['videoLinks']['download']
-    directory = str(round(time.time()))
-    filename = str(int(time.time()))+'.mp4'
-    size = int(requests.head(r).headers['Content-length'])
-    total_size = "{:.2f}".format(int(size) / 1048576)
-    try:
-        os.mkdir(directory)
-    except:
-        pass
-    with requests.get(r, timeout=(50, 10000), stream=True) as r:
-        r.raise_for_status()
-        with open(f'./{directory}/{filename}', 'wb') as f:
-            chunk_size = 1048576
-            dl = 0
-            show = 1
-            for chunk in r.iter_content(chunk_size=chunk_size):
-                f.write(chunk)
-                dl = dl + chunk_size
-                percent = round(dl * 100 / size)
-                if percent > 100:
-                    percent = 100
-                if show == 1:
-                    try:
-                        a.edit(f'__**URL :**__ __{message.text}__\n'
-                               f'__**Total Size :**__ __{total_size} MB__\n'
-                               f'__**Downloaded :**__ __{percent}%__\n',
-                               disable_web_preview=False)
-                    except:
-                        pass
-                    if percent == 100:
-                        show = 0
-
-        a.edit(f'__Downloaded to the server!\n'
-               f'Uploading to Telegram Now ⏳__')
-        start = time.time()
-        title = filename
-        app.send_document(chat_id=message.chat.id,
-                          document=f"./{directory}/{filename}",
-                          caption=f"**File :** __{filename}__\n"
-                          f"**Size :** __{total_size} MB__\n\n"
-                          f"__Uploaded by @{BOT_URL}__",
-                          file_name=f"{directory}",
-                          parse_mode='md',
-                          progress=progress,
-                          progress_args=(a, start, title))
-        a.delete()
-        try:
-            shutil.rmtree(directory)
-        except:
-            pass
-
-
-app.run()
+# Start the bot
+updater.start_polling()
